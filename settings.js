3978dab5ef2537098ad619dc96d6b559b865077b
pastor101-afk <gospelodey9@gmail.com>
2025-05-14 14:12:23 +0100

    .

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || 'KANGO~nQkQVKAT#UV5iI7qXWYVjFsEZVsK0YAiuyIdlVoH2eW3fym5WZQY' 
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~SQdgybKI#eGDjcYTiIUnHB75mFz2z8uMLmwX6g-CbJfFHvTmV18I' 
 //Enter your 𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿 session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
